0ed62f027226cc1fc09e3257bd7f092735719916
Dev Goswami <mahadevchhaganpuri@gmail.com>
2026-01-31 18:28:41 +0000

    Datatypes

---
+++ 01_Variables.js
@@ -0,0 +1,15 @@
+const accountID = 144554
+let accountEmail = "dev@gmail.com"
+var accountPassowrd = "12456"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 2
+
+accountEmail = "dev@dev.com"
+accountPassowrd = "222222"
+accountCity = "Lahore"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassowrd, accountCity, accountState]);
---
+++ Datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+// alert("hello");
+// alert(3 + 3); // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "dev";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to the power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique 
+
+// object 
+
+console.log(typeof "Hitesh")
