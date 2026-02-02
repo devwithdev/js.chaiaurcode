65f4e6218a2b8315f7dfa08b4894903e42e77fcb
Dev Goswami <mahadevchhaganpuri@gmail.com>
2026-02-02 14:05:20 +0000

    comparison done

---
+++ Basics/03_conversion.js
@@ -0,0 +1,87 @@
+// CONVERSIONS IN JAVASCRIPT
+let score = "abc";
+
+// console.log(typeof score );
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+//Notes: 
+// "33" => 33
+// "33 abc" => NaN
+// "Undifined" => NaN
+// "Null" => 0
+// "Ture" => 1; "false" => 0
+
+
+
+let isLoggedIn = "f";
+
+let inBoolean = Boolean(isLoggedIn);
+//console.log(inBoolean);
+
+// "1" => true
+// "0" => false
+// "" => false
+// "Dev" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*************************** OPERATIONS ********************************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " dev";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+let numb1, numb2, numb3;
+numb1 = numb2 = numb3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ comparison.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// Note: Always compare values which have same datatypes. Never compare different datatypes.
+// These types of conversions which have different datatypes are alwyas confusing.
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === : (strict check) checks values strictly. it also checks datatypes.
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
+
